bb4de593440c34c319396a80ab5401217517b39b
jhon patiño <jhon@gmail.com>
2024-03-20 23:16:52 -0500

    creacion del proyecto y maquetado en html y css

---
+++ src/CartApp.jsx
@@ -0,0 +1,72 @@
+export const CarApp = () =>{
+
+     return (<> 
+      <h3>Cart App</h3>
+        <div  className="container">
+        <div className="row">
+            <div className="col-4 my-2">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">teclado mecanico</h5>
+                        <p className="card-text">teclado mecanico con luces rgb switches red</p>
+                        <p className="card-text">$ 100</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-4 my-2">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Samsung Smartv</h5>
+                        <p className="card-text">teclado mecanico con luces rgb switches red</p>
+                        <p className="card-text">$ 1000</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-4 my-2">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Audifono </h5>
+                        <p className="card-text">teclado mecanico con luces rgb switches red</p>
+                        <p className="card-text">$ 500</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-4 my-2">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Laptop asus</h5>
+                        <p className="card-text">teclado mecanico con luces rgb switches red</p>
+                        <p className="card-text">$ 3000</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-4 my-2">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Cpu intel i5</h5>
+                        <p className="card-text">teclado mecanico con luces rgb switches red</p>
+                        <p className="card-text">$ 1500</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-4 my-2">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Laptop Hp</h5>
+                        <p className="card-text">teclado mecanico con luces rgb switches red</p>
+                        <p className="card-text">$ 2000</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        </div>
+     </>)
+}
